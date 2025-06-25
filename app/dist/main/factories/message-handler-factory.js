ee013905a29bfac686bf9b5e891b2e875b55fea3
Douglas Volcato <douglasvolcato@gmail.com>
2025-06-25 03:31:05 +0000

    chore: add whatsapp log

--- app/dist/main/factories/message-handler-factory.js
+++ app/dist/main/factories/message-handler-factory.js
@@ -6,6 +6,7 @@ const insert_user_last_message_usecase_1 = require("../../domain/usecases/chat/i
 const get_user_by_number_usecase_1 = require("../../domain/usecases/users/get-user-by-number-usecase");
 const llm_agent_1 = require("../../ia/agents/llm-agent");
 const llm_agent_factory_1 = require("./llm-agent-factory");
+const logger_1 = require("../../domain/utils/logger");
 function makeMessageHandler() {
     const getUserByNumberUseCase = new get_user_by_number_usecase_1.GetUserByNumberUseCase.Service();
     const getUserLastMessagesUseCase = new get_user_last_messages_usecase_1.GetUserLastMessagesUseCase.Service();
@@ -14,46 +15,55 @@ function makeMessageHandler() {
         return `*Assistente*: ${content}`;
     }
     async function handleMessages(input) {
-        const user = await getUserByNumberUseCase.execute({ number: input.number });
-        for (const message of input.lastMessages) {
+        try {
+            logger_1.Logger.writeLog("logs/whatsapp.txt", `[MESSAGE] ${input.number} - ${JSON.stringify(input.lastMessages[input.lastMessages.length - 1])}`);
+            const user = await getUserByNumberUseCase.execute({ number: input.number });
+            for (const message of input.lastMessages) {
+                await insertUserLastMessageUseCase.execute({
+                    user_id: user.id,
+                    sender: message.sender,
+                    content: message.content,
+                });
+            }
+            const lastMessages = await getUserLastMessagesUseCase.execute({
+                user_id: user.id,
+            });
+            const agent = await new llm_agent_factory_1.LlmAgentFactory({
+                user: user,
+            }).getAgent();
+            for (const message of lastMessages) {
+                agent.addMessage({
+                    content: message.content,
+                    role: message.sender,
+                });
+            }
+            const response = await agent.getResponse();
+            const newConversationState = await getUserByNumberUseCase.execute({ number: input.number });
+            if (newConversationState.conversation_state !== user.conversation_state) {
+                return await handleMessages({
+                    number: input.number,
+                    lastMessages: [
+                        ...lastMessages,
+                        {
+                            content: formatWhatsappMessage(response.response),
+                            sender: llm_agent_1.MessageSenderEnum.ASSISTANT,
+                        },
+                    ],
+                });
+            }
             await insertUserLastMessageUseCase.execute({
                 user_id: user.id,
-                sender: message.sender,
-                content: message.content,
+                sender: llm_agent_1.MessageSenderEnum.ASSISTANT,
+                content: formatWhatsappMessage(response.response),
             });
+            const formatedResponse = formatWhatsappMessage(response.response);
+            logger_1.Logger.writeLog("logs/whatsapp.txt", `[REPLY] ${formatedResponse}`);
+            return formatedResponse;
         }
-        const lastMessages = await getUserLastMessagesUseCase.execute({
-            user_id: user.id,
-        });
-        const agent = await new llm_agent_factory_1.LlmAgentFactory({
-            user: user,
-        }).getAgent();
-        for (const message of lastMessages) {
-            agent.addMessage({
-                content: message.content,
-                role: message.sender,
-            });
+        catch (error) {
+            logger_1.Logger.error(error);
+            return null;
         }
-        const response = await agent.getResponse();
-        const newConversationState = await getUserByNumberUseCase.execute({ number: input.number });
-        if (newConversationState.conversation_state !== user.conversation_state) {
-            return await handleMessages({
-                number: input.number,
-                lastMessages: [
-                    ...lastMessages,
-                    {
-                        content: formatWhatsappMessage(response.response),
-                        sender: llm_agent_1.MessageSenderEnum.ASSISTANT,
-                    },
-                ],
-            });
-        }
-        await insertUserLastMessageUseCase.execute({
-            user_id: user.id,
-            sender: llm_agent_1.MessageSenderEnum.ASSISTANT,
-            content: formatWhatsappMessage(response.response),
-        });
-        return formatWhatsappMessage(response.response);
     }
     return handleMessages;
 }
--- app/src/main/factories/message-handler-factory.ts
+++ app/src/main/factories/message-handler-factory.ts
@@ -4,6 +4,7 @@ import { GetUserByNumberUseCase } from "../../domain/usecases/users/get-user-by-
 import { ChatMessage } from "../../domain/utils/whatsapp-interactor";
 import { MessageSenderEnum } from "../../ia/agents/llm-agent";
 import { LlmAgentFactory } from "./llm-agent-factory";
+import { Logger } from "../../domain/utils/logger";
 
 export function makeMessageHandler() {
   const getUserByNumberUseCase = new GetUserByNumberUseCase.Service();
@@ -18,58 +19,69 @@ export function makeMessageHandler() {
     number: string;
     lastMessages: ChatMessage[];
   }): Promise<string | null> {
-    const user: typeof GetUserByNumberUseCase.Output =
-      await getUserByNumberUseCase.execute({ number: input.number });
+    try {
+      Logger.writeLog(
+        "logs/whatsapp.txt",
+        `[MESSAGE] ${input.number} - ${JSON.stringify(input.lastMessages[input.lastMessages.length - 1])}`,
+      );
+      const user: typeof GetUserByNumberUseCase.Output =
+        await getUserByNumberUseCase.execute({ number: input.number });
 
-    for (const message of input.lastMessages) {
+      for (const message of input.lastMessages) {
+        await insertUserLastMessageUseCase.execute({
+          user_id: user.id,
+          sender: message.sender,
+          content: message.content,
+        });
+      }
+
+      const lastMessages: typeof GetUserLastMessagesUseCase.Output =
+        await getUserLastMessagesUseCase.execute({
+          user_id: user.id,
+        });
+
+      const agent = await new LlmAgentFactory({
+        user: user,
+      }).getAgent();
+
+      for (const message of lastMessages) {
+        agent.addMessage({
+          content: message.content,
+          role: message.sender,
+        });
+      }
+
+      const response = await agent.getResponse();
+
+      const newConversationState: typeof GetUserByNumberUseCase.Output =
+        await getUserByNumberUseCase.execute({ number: input.number });
+
+      if (newConversationState.conversation_state !== user.conversation_state) {
+        return await handleMessages({
+          number: input.number,
+          lastMessages: [
+            ...lastMessages,
+            {
+              content: formatWhatsappMessage(response.response),
+              sender: MessageSenderEnum.ASSISTANT,
+            },
+          ],
+        });
+      }
+
       await insertUserLastMessageUseCase.execute({
         user_id: user.id,
-        sender: message.sender,
-        content: message.content,
+        sender: MessageSenderEnum.ASSISTANT,
+        content: formatWhatsappMessage(response.response),
       });
+
+      const formatedResponse = formatWhatsappMessage(response.response);
+      Logger.writeLog("logs/whatsapp.txt", `[REPLY] ${formatedResponse}`);
+      return formatedResponse;
+    } catch (error: any) {
+      Logger.error(error);
+      return null;
     }
-
-    const lastMessages: typeof GetUserLastMessagesUseCase.Output =
-      await getUserLastMessagesUseCase.execute({
-        user_id: user.id,
-      });
-
-    const agent = await new LlmAgentFactory({
-      user: user,
-    }).getAgent();
-
-    for (const message of lastMessages) {
-      agent.addMessage({
-        content: message.content,
-        role: message.sender,
-      });
-    }
-
-    const response = await agent.getResponse();
-
-    const newConversationState: typeof GetUserByNumberUseCase.Output =
-      await getUserByNumberUseCase.execute({ number: input.number });
-
-    if (newConversationState.conversation_state !== user.conversation_state) {
-      return await handleMessages({
-        number: input.number,
-        lastMessages: [
-          ...lastMessages,
-          {
-            content: formatWhatsappMessage(response.response),
-            sender: MessageSenderEnum.ASSISTANT,
-          },
-        ],
-      });
-    }
-
-    await insertUserLastMessageUseCase.execute({
-      user_id: user.id,
-      sender: MessageSenderEnum.ASSISTANT,
-      content: formatWhatsappMessage(response.response),
-    });
-
-    return formatWhatsappMessage(response.response);
   }
 
   return handleMessages;
